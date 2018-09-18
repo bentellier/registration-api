435bd945b2b3d7505794ad96b5aae8736ed53319
Ben T <benjamin.tellier@feed.xyz>
2018-09-18 16:52:05 +0100

    update on cors app.use(cors()); covers everything needed

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ const cors = require ("cors");
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use("/registration-server", (req, res) => {
   const visitorName = `${req.query["visitor-name"]}` || "NO NAME GIVEN :O";
   console.log("Name:", visitorName);
