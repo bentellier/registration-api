0681efcef11a282067d19db2a54a4bb35ecf4b32
Ben T <benjamin.tellier@feed.xyz>
2018-09-18 15:51:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require ("cors");
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use("/registration-server", (req, res) => {
+  const visitorName = `${req.query["visitor-name"]}` || "NO NAME GIVEN :O";
+  console.log("Name:", visitorName);
+  const visitorCompany = `${req.query["visitor-company"]}` || "NO COMPANY GIVEN :O";
+  console.log("Company:", visitorCompany);
+  const visitedName = `${req.query["visited-name"]}` || "NO VISITED NAME GIVEN :O";
+  console.log("Visited name:", visitedName);
+  const reasonSelection = `${req.query["reason-selection"]}` || "NO NAME GIVEN :O";
+  console.log("Reason for visiting:", reasonSelection);
+  res.send(`REQUEST FOR: <br />Name: ${visitorName}<br /> Company: ${visitorCompany}<br /> Visited name: ${visitedName}<br /> Reason for visiting: ${reasonSelection}`);
+});
+
+app.listen(8888, () => {
+  console.log(`server started at port 8888`);
+});
