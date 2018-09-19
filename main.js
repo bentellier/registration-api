279391cb3cdcf52337a2e65edb95592d2de1ae40
Ben T <benjamin.tellier@feed.xyz>
2018-09-19 12:20:14 +0100

    Removed consol.log from the api

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ app.use(cors());
 
 app.use("/registration-server", (req, res) => {
   const visitorName = `${req.query["visitor-name"]}` || "NO NAME GIVEN :O";
-  console.log("Name:", visitorName);
+  // console.log("Name:", visitorName);
   const visitorCompany = `${req.query["visitor-company"]}` || "NO COMPANY GIVEN :O";
-  console.log("Company:", visitorCompany);
+  // console.log("Company:", visitorCompany);
   const visitedName = `${req.query["visited-name"]}` || "NO VISITED NAME GIVEN :O";
-  console.log("Visited name:", visitedName);
+  // console.log("Visited name:", visitedName);
   const reasonSelection = `${req.query["reason-selection"]}` || "NO NAME GIVEN :O";
-  console.log("Reason for visiting:", reasonSelection);
+  // console.log("Reason for visiting:", reasonSelection);
   res.send(`REQUEST FOR: <br />Name: ${visitorName}<br /> Company: ${visitorCompany}<br /> Visited name: ${visitedName}<br /> Reason for visiting: ${reasonSelection}`);
 });
 
