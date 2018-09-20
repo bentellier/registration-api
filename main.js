4d6c42803eb90e96c05568ecdeb61dcbcd8fe417
Ben T <benjamin.tellier@feed.xyz>
2018-09-20 10:54:21 +0100

    adding back console.log in the script

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const cors = require ("cors");
 app.use(cors());
 
 app.use("/registration-server", (req, res) => {
-  const visitorName = `${req.query["visitor-name"]}` || "NO NAME GIVEN :O";
-  // console.log("Name:", visitorName);
-  const visitorCompany = `${req.query["visitor-company"]}` || "NO COMPANY GIVEN :O";
-  // console.log("Company:", visitorCompany);
-  const visitedName = `${req.query["visited-name"]}` || "NO VISITED NAME GIVEN :O";
-  // console.log("Visited name:", visitedName);
-  const reasonSelection = `${req.query["reason-selection"]}` || "NO NAME GIVEN :O";
-  // console.log("Reason for visiting:", reasonSelection);
-  res.send(`REQUEST FOR: <br />Name: ${visitorName}<br /> Company: ${visitorCompany}<br /> Visited name: ${visitedName}<br /> Reason for visiting: ${reasonSelection}`);
+  const visitorName = `${req.query["name"]}` || "NO NAME GIVEN :O";
+  console.log("Name:", visitorName);
+  const visitorCompany = `${req.query["company"]}` || "NO COMPANY GIVEN :O";
+  console.log("Company:", visitorCompany);
+  const visitedName = `${req.query["visited"]}` || "NO VISITED NAME GIVEN :O";
+  console.log("Visited name:", visitedName);
+  const reasonSelection = `${req.query["reason"]}` || "NO NAME GIVEN :O";
+  console.log("Reason for visiting:", reasonSelection);
+  res.send(`REGISTERED: <br />Name: ${visitorName}<br /> Company: ${visitorCompany}<br /> Visited name: ${visitedName}<br /> Reason for visiting: ${reasonSelection}`);
 });
 
 app.listen(8888, () => {
